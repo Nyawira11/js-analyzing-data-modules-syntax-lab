1121e349c786a5bd360033a6f085562cd607a27a
nyawira11 <nyawiradan5@gmail.com>
2026-04-17 11:48:20 +0300

    add js code

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
+require("datejs" );
 
+function combineUsers(...args) {
+   const combinedObject = {
+      users: []
+   };
+
+   args.forEach(arr => {
+    combinedObject.users = [...combinedObject.users, ...arr];
+   });
+
+  combinedObject.merge_date = new Date().toString("M/d/yyyy");
+
+  return combinedObject;
+}
+
+
+module.exports = {
+  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+};
 
 
 module.exports = {
